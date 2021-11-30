3004a60f6bd26373ddbb91485d68d6d02e910e0e
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2021-11-30 10:20:32 +0530

    First commit

--- src/com/company/classes/DriverClass.java
+++ src/com/company/classes/DriverClass.java
@@ -2,12 +2,14 @@ package com.company.classes;
 
 public class DriverClass {
     public static void main(String[] args){
-        house House = new house();
+        house House = new house(20, 10, "tiles", "plaster" );
         System.out.println(House.getnOfDoors());
 
         House.setnOfDoors(10);
         House.setnOfWindows(20);
         System.out.println("Number of Doors : " + House.getnOfDoors());
         System.out.println("Number of Windows : " + House.getnOfWindows());
+        System.out.println("Type of Roof : " + House.getTypeOfRoof());
+        System.out.println("Type of Wall : " + House.getTypeOfWall());
     }
 }
